aed0216199fda9b3a903199ec34aa3f3917dcacb
wwkd623 <wwkd623@naver.com>
2025-06-17 17:21:49 +0900

    Add files via upload

--- 04_familysite/js/ibk.js
+++ 04_familysite/js/ibk.js
@@ -1,8 +1,10 @@
 $(function(){
     $(".footer .inner .group .family1 button").click(function(){
         $(".footer .inner .group .family1 ul").toggle();
+        $(".footer .inner .group .family2 ul").hide();
     })
     $(".footer .inner .group .family2 button").click(function(){
         $(".footer .inner .group .family2 ul").toggle();
+        $(".footer .inner .group .family1 ul").hide();
     })
 })
