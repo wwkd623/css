8dc5516454035d288dbc5ba71820545e9200eeca
wwkd623 <wwkd623@naver.com>
2025-06-18 17:32:46 +0900

    Add files via upload

--- 05_slide_coffeemama/js/main.js
+++ 05_slide_coffeemama/js/main.js
@@ -2,6 +2,8 @@ $(function(){
     $(".slider").slick({
         autoplay: true,
         // fadein/fadeout슬라이드 하는 법
-        fade: true
+        fade: true,
+        // 페이지버튼 보이게 하기
+        dots: true
     });
 })
