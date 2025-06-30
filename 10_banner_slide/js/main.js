ad28e75e9649a71fccf4d009aebd057176eec687
wwkd623 <wwkd623@naver.com>
2025-06-30 16:42:52 +0900

    Add files via upload

--- 10_banner_slide/js/main.js
+++ 10_banner_slide/js/main.js
@@ -9,4 +9,19 @@ $(function(){
         arrows: false,
         dots: true,
     });
+    // $(".btn_pause").click(function(){
+    //     $(".btn_pause").toggleClass("on")
+    // })
+    let sw = true;
+    $(".btn_pause").click(function(){
+        if(sw==true){
+            $(".btn_pause").addClass("on");
+            $(".list").slick("slickPause");
+            sw = false;
+        }else{
+            $(".btn_pause").removeClass("on");
+            $(".list").slick("slickPlay");
+            sw = true;
+        }
+    })
 });
