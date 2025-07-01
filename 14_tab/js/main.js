b686c47360ea8e30e5e7751a1b91294e030e395e
wwkd623 <wwkd623@naver.com>
2025-07-01 16:42:19 +0900

    Add files via upload

--- 14_tab/js/main.js
+++ 14_tab/js/main.js
@@ -1,6 +1,11 @@
 $(function(){
+    $(".list2").hide();
     $(".section .inner .right .wrap .menu li").click(function(){
         $(".section .inner .right .wrap .menu li").removeClass("on")
         $(this).addClass("on")
+        let num = $(this).index();
+        console.log(num)
+        $(".list").hide();
+        $(".list").eq(num).show();
     })
 })
