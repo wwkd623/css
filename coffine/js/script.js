c6246dca5bc34d1350428c21227606bbb3c5321d
wwkd623 <wwkd623@naver.com>
2025-07-22 10:12:54 +0900

    Add files via upload

--- coffine/js/script.js
+++ coffine/js/script.js
@@ -21,3 +21,37 @@ $(document).ready(function(){
     dots: true,
   });
 });
+
+// 배너 슬라이드
+$(document).ready(function(){
+  $('.banner_slide').slick({
+    autoplay: true,
+    dots: true,
+    arrows: false,
+  });
+});
+
+// 탭 기능
+$(function(){
+  $("ul.tab li").click(function(e){
+    // 클릭하면 위로 올라가는거 방지
+    e.preventDefault();
+    $("ul.tab li").removeClass("on");
+    $(this).addClass("on");
+  })
+
+  // 리스트박스 클릭하면 on클래스
+  $(".content ul.list li").click(function(e){
+    e.preventDefault();
+    $(".content ul.list li").removeClass("on");
+    $(this).addClass("on");
+  })
+
+  // new_menu on클래스
+  $(".content ul.new_menu li").click(function(e){
+    e.preventDefault();
+    $(".content ul.new_menu li").removeClass("on");
+    $(this).addClass("on");
+  })
+
+})
